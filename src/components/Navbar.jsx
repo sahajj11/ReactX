ca3f60e74ef22395f1add079da020e0eb91b42bc
Sahaj Rajput <sahajrajput1112@gmail.com>
2024-09-27 23:04:39 +0530

    adding files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav class="navbar  navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid d-flex align-items-center">
+   <h3 className='mx-4 mb-0'>ReactX</h3>
+
+   <p className='fs-5 me-5 mb-0'>Generate React Components Effortlessly</p>
+    
+   
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
